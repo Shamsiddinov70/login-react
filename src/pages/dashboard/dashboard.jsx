bbe356e97d34846d101a2b7161b07763b658a9d7
Shamsiddinov70 <abbosshamsiddin@gmail.com>
2022-07-16 17:29:04 +0500

    add posts

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
 export const Dashboard = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        (async function () {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+            console.log(res);
+
+            if (!res.ok) {
+                throw new Error(JSON.stringify(res));
+            }
+
+            const data = await res.json();
+            setPosts(data)
+        })()
+    }, [])
+    console.log(posts);
+
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
+      <div className="main">
+          <div className="container">
+            <h1 className="text-center mb-5 mt-2 text-info">POSTS</h1>
+                {posts.map(post => (
+                    <li className="text-center border w-50 mx-auto p-2 list-unstyled mb-3">
+                        <h3 className="" key={post.id}>{post.title}</h3>
+                        <p className="text-truncate">{post.body}</p>
+                    </li>
+                ))}
+          </div>
+      </div>
+    );
 };
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,8 +3,8 @@ import {Link} from 'react-router-dom';
 export const Home = () => {
     return (
         <div>
-            <h1>Home page me</h1>
-            <Link to="/login">Sign in</Link>
+            <h1 className='my-5 ms-5'>Welcome,Home page</h1>
+            <Link className='link border rounded-3' to="/login">Sign in</Link>
         </div>
     )
 };
